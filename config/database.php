40c0d65089ddde95d7a424f7c655ad9fec5a3cd1
jeremy <jeremyw0815@icloud.com>
2026-02-15 13:10:54 -0500

    Added fix to check whether the newer Pdo\Mysql::ATTR_INIT_COMMAND constant exists and falls back to the old PDO::MYSQL_ATTR_INIT_COMMAND if not

--- config/database.php
+++ config/database.php
@@ -44,11 +44,15 @@ if ($isLocal) {
 $dbConfig['charset']   = 'utf8mb4';
 $dbConfig['collation'] = $isLocal ? 'utf8mb4_0900_ai_ci' : 'utf8mb4_unicode_ci';
 
+$mysqlInitCmd = defined('Pdo\Mysql::ATTR_INIT_COMMAND')
+    ? Pdo\Mysql::ATTR_INIT_COMMAND
+    : PDO::MYSQL_ATTR_INIT_COMMAND;
+
 $dbConfig['options'] = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES   => false,
-    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4 COLLATE " . ($isLocal ? 'utf8mb4_0900_ai_ci' : 'utf8mb4_unicode_ci'),
+    $mysqlInitCmd                => "SET NAMES utf8mb4 COLLATE " . ($isLocal ? 'utf8mb4_0900_ai_ci' : 'utf8mb4_unicode_ci'),
 ];
 
 // Build DSN

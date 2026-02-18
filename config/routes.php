1a413148cb098669e777cf9184d4d1826058ea55
jeremy <jeremyw0815@icloud.com>
2026-02-18 12:51:15 -0500

    Activated POST /borrow{id}/approve route

--- config/routes.php
+++ config/routes.php
@@ -65,7 +65,7 @@ return [
 
     // Borrowing
     'POST /borrow/request'         => [BorrowController::class, 'request'],
-    // 'POST /borrow/{id}/approve'    => [BorrowController::class, 'approve'],
+    'POST /borrow/{id}/approve'    => [BorrowController::class, 'approve'],
     // 'POST /borrow/{id}/deny'       => [BorrowController::class, 'deny'],
     // 'POST /borrow/{id}/cancel'     => [BorrowController::class, 'cancel'],
     // 'POST /borrow/{id}/return'     => [BorrowController::class, 'return'],

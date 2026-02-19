c57f8beb156bbc4b15a5a6ceb93c5976e2829e55
jeremy <jeremyw0815@icloud.com>
2026-02-19 17:46:58 -0500

    Activated GET/disputes/create/{borrowId} route

--- config/routes.php
+++ config/routes.php
@@ -91,7 +91,7 @@ return [
 
     // Disputes
     'GET /disputes'                => [DisputeController::class, 'index'],
-    // 'GET /disputes/create/{borrowId}' => [DisputeController::class, 'create'],
+    'GET /disputes/create/{borrowId}' => [DisputeController::class, 'create'],
     // 'POST /disputes'               => [DisputeController::class, 'store'],
     // 'GET /disputes/{id}'           => [DisputeController::class, 'show'],
     // 'POST /disputes/{id}/message'  => [DisputeController::class, 'addMessage'],

dd5f51109ffc2aeb6bf793586bcd00959fb6255a
jeremy <jeremyw0815@icloud.com>
2026-03-20 10:55:14 -0400

    Enabled persistent DB connections

--- config/database.php
+++ config/database.php
@@ -22,6 +22,7 @@ $dbConfig['options'] = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES   => false,
+    PDO::ATTR_PERSISTENT         => true,
     $mysqlInitCmd                => "SET NAMES utf8mb4 COLLATE {$collation}",
 ];
 

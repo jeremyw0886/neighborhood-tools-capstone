2658f103b688b371e8f5e6b1caf6e539c4074f9f
jeremy <jeremyw0815@icloud.com>
2026-03-20 11:48:33 -0400

    Fixed html validation error

--- src/Views/tools/index.php
+++ src/Views/tools/index.php
@@ -268,7 +268,7 @@ $paginationUrl = static function (int $pageNum) use ($filterParams, $basePath): 
     <?php endif; ?>
 
     <section aria-label="No results" <?= $hasTools ? 'hidden' : '' ?>>
-      <img src="/assets/images/empty-search.svg" alt="" width="200" height="200" role="presentation">
+      <img src="/assets/images/empty-search.svg" alt="" width="200" height="200">
       <h2>No Tools Found</h2>
       <?php if (!empty($filterParams)): ?>
         <ul>
